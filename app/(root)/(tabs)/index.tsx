830036a82a2b2e918b043386b56c630b1e7d1878
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2025-02-06 01:09:39 +0530

    Update configuration, add image and icon modules, and enhance sign-in screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -8,8 +8,10 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        height: "100%",
       }}
     >
+      <Text className=" p-5 text-4xl font-rubik">Welcome Harshit</Text>
       <Link href="/sign-in"> Sign In</Link>
       <Link href="/explore"> Explore</Link>
       <Link href="/profile"> Profile</Link>
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = ()=>{};
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full" >
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="flex items-center justify-center">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To Real Scout
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer To {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-6">
+            Login to Real Scout with Google
+          </Text>
 
-export default SignIn
+            <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-md w-[90%] py-4 mt-6 border border-zinc-100">
+            <View className="flex flex-row items-center justify-center ">
+              <Image source={icons.google} className="w-5 h-5" resizeMode="contain"/>
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+              Continue with Google
+              </Text>
+            </View>
+            </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
